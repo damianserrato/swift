ef306202d2c849e9d3d1fb2594334f4e471cff6f
damianserrato <33769904+damianserrato@users.noreply.github.com>
2019-02-20 13:58:09 -0500

    Modal Segues
    First Commit using Modal Segues

---
+++ ModalSegues/ModalSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModalSegues
+//
+//  Created by Conner on 2/20/19.
+//  Copyright © 2019 damianserrato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ModalSegues/ModalSegues/otherView.swift
@@ -0,0 +1,21 @@
+//
+//  otherView.swift
+//  ModalSegues
+//
+//  Created by Conner on 2/20/19.
+//  Copyright © 2019 damianserrato. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class otherView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func dismissButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
